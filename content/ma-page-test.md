ec1b892bc14f82899da45569f737dbea47899bac
ngawangtendor <nicolathomas1999@gmail.com>
2024-05-21 16:35:37 +0200

    pelica111

---
+++ content/ma-page-test.md
@@ -0,0 +1,26 @@
+Title: Ma première page
+Date: 2023-05-30 18:00
+Tags: Test, démonstration
+Category: Essai
+Authors: professeur CPI
+Summary: Une 1ère page
+Voici la 1ère ligne de ma page.
+# Le 1er titre de 1er niveau (H1) :
+## Le titre de 2e niveau (H2) :
+Cette page est une simple démonstation. 
+Qui contient **quelques éléments**
+<img src="images/why.jpg">
+Une liste **non énumérée** :
+- Ligne 1
+- Ligne 2
+
+Une liste **énumérée** :
+
+1. En 1er
+2. En 2e
+
+Un tableau :
+| Syntax | Description |
+| ----------- | ----------- |
+| Header | Title |
+| Paragraph | Text |
---
+++ pelicanconf.py
@@ -0,0 +1,39 @@
+AUTHOR = 'Equipe CPI'
+SITENAME = 'AugustineTeam'
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = 'fr'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+#image
+STATIC_PATHS = ['images']
+
+# Blogroll
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    ("You can modify those links in your config file", "#"),
+)
+
+# Social widget
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+THEME = 'nest-master'
